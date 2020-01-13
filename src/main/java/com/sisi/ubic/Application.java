5a8ea1c4f64b26d1113e3e429f8fddfca62609c7
Sisi <dqgsh1055@gmail.com>
2020-01-13 21:23:32 +0900

    JPA Auditing 으로 생성시간/수정시간 자동화

--- src/main/java/com/sisi/ubic/Application.java
+++ src/main/java/com/sisi/ubic/Application.java
@@ -2,6 +2,7 @@ package com.sisi.ubic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // @SpringBootApplication 으로 인해
 // 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // 특히나 @SpringBootApplication 이 있는 위치부터
 // 설정을 읽어가기 때문에
 // 이 클래스는 항상 프로젝트 최상단에 위치해야 한다.
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     // main 메소드에서 실행하는 SpringApplication.run 으로 인해
